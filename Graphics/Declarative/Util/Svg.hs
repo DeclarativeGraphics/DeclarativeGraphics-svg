82436fa1d1c66c1f785a46ab7f88663b74d2688a
Daniel Krueger <keenbug@gmail.com>
2014-12-29 01:57:18 +0100

    adopted Graphic and Frame refactoring

--- Graphics/Declarative/Util/Svg.hs
+++ Graphics/Declarative/Util/Svg.hs
@@ -6,26 +6,27 @@ import Graphics.Rendering.Cairo.SVG
 import Graphics.Rendering.Cairo (liftIO)
 
 import Graphics.Declarative.Graphic
-import Graphics.Declarative.Envelope
-import Graphics.Declarative.Enveloped
+import Graphics.Declarative.Frame
+import Graphics.Declarative.Framed
 import Graphics.Declarative.Backend.Cairo
 
 -- Origin on top-left
-svgForm :: SVG -> CairoEGraphic
-svgForm svg =
-  let (w, h) = svgGetSize svg
-   in Pack (Envelope 0 0 (fromIntegral w) (fromIntegral h)) $ Leaf $ do
-    success <- svgRender svg
-    unless success $ liftIO $ putStrLn "Warning: Couldn't render SVG. (Graphics.Declarative.Util.SVG)"
+svgForm :: SVG -> Form
+svgForm svg
+  = Framed (Frame 0 0 (fromIntegral w) (fromIntegral h)) $ primitive $ do
+      success <- svgRender svg
+      unless success $ liftIO $
+        putStrLn "Warning: Couldn't render SVG. (Graphics.Declarative.Util.SVG)"
+  where (w, h) = svgGetSize svg
 
 -- Origin on top-left
-svgFormString :: String -> IO CairoEGraphic
+svgFormString :: String -> IO Form
 svgFormString str = svgForm `fmap` svgNewFromString str
 
 -- Origin on top-left
-svgFormHandle :: Handle -> IO CairoEGraphic
+svgFormHandle :: Handle -> IO Form
 svgFormHandle handle = svgForm `fmap` svgNewFromHandle handle
 
 -- Origin on top-left
-svgFormFile :: FilePath -> IO CairoEGraphic
+svgFormFile :: FilePath -> IO Form
 svgFormFile file = svgForm `fmap` svgNewFromFile file
