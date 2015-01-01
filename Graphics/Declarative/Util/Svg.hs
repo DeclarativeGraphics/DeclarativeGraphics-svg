69633816f0a0e59952b928857931300a36a890e9
Philipp Krüger <matheus.dev@gmail.com>
2015-01-01 16:23:01 +0100

    Rewrite

--- Graphics/Declarative/Util/Svg.hs
+++ Graphics/Declarative/Util/Svg.hs
@@ -6,14 +6,14 @@ import Graphics.Rendering.Cairo.SVG
 import Graphics.Rendering.Cairo (liftIO)
 
 import Graphics.Declarative.Graphic
-import Graphics.Declarative.Frame
-import Graphics.Declarative.Framed
+import Graphics.Declarative.Border
+import Graphics.Declarative.Bordered
 import Graphics.Declarative.Backend.Cairo
 
 -- Origin on top-left
 svgForm :: SVG -> Form
 svgForm svg
-  = Framed (Frame 0 0 (fromIntegral w) (fromIntegral h)) $ primitive $ do
+  = Bordered (fromFrame (0, 0, (fromIntegral w), (fromIntegral h))) $ primitive $ do
       success <- svgRender svg
       unless success $ liftIO $
         putStrLn "Warning: Couldn't render SVG. (Graphics.Declarative.Util.SVG)"
